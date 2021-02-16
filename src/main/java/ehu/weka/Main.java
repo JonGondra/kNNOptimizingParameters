ff418def40d2e08c0c765bc526f46caa0ed4cdb9
Jon Gondra <jongondra22@gmail.com>
2021-02-16 13:26:49 +0100

    argumentuekin funtzionatu.

--- src/main/java/ehu/weka/Main.java
+++ src/main/java/ehu/weka/Main.java
@@ -14,13 +14,17 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        //datuak kargatu
-        String path = "C:\\Users\\jongo\\OneDrive\\Escritorio\\3.Praktika\\balance-scale.arff";
-        ConverterUtils.DataSource source = new ConverterUtils.DataSource(path);
-        Instances data = source.getDataSet();
-        data.setClassIndex(data.numAttributes()-1);
+        if (args.length != 1) {
+            System.out.println("\nUsage: kNN </path/data.arff>\n");
+        } else {
+            //datuak kargatu
+            //String path = "C:\\Users\\jongo\\OneDrive\\Escritorio\\3.Praktika\\balance-scale.arff";
+            String path = args[0];
+            ConverterUtils.DataSource source = new ConverterUtils.DataSource(path);
+            Instances data = source.getDataSet();
+            data.setClassIndex(data.numAttributes() - 1);
 
-        //Cross Validation erabiliko ez balitz:
+            //Cross Validation erabiliko ez balitz:
         /*
         //Randomize
         Randomize filter = new Randomize();
@@ -46,50 +50,51 @@ public class Main {
         System.out.println("\n\nTest instantzia kopurua: "+test.numInstances());
          */
 
-        //Bilaketak eta pisuak kargatu
-        LinearNNSearch manDistance =new LinearNNSearch();
-        manDistance.setDistanceFunction(new ManhattanDistance());
-        LinearNNSearch euDistance =new LinearNNSearch();
-        manDistance.setDistanceFunction(new EuclideanDistance());
-        LinearNNSearch cheDistance =new LinearNNSearch();
-        manDistance.setDistanceFunction(new ChebyshevDistance());
-        LinearNNSearch filDistance =new LinearNNSearch();
-        manDistance.setDistanceFunction(new FilteredDistance());
-        LinearNNSearch minkDistance =new LinearNNSearch();
-        manDistance.setDistanceFunction(new MinkowskiDistance());
-        LinearNNSearch[] distances = new LinearNNSearch[] {manDistance,euDistance,cheDistance,filDistance,minkDistance};
+            //Bilaketak eta pisuak kargatu
+            LinearNNSearch manDistance = new LinearNNSearch();
+            manDistance.setDistanceFunction(new ManhattanDistance());
+            LinearNNSearch euDistance = new LinearNNSearch();
+            manDistance.setDistanceFunction(new EuclideanDistance());
+            LinearNNSearch cheDistance = new LinearNNSearch();
+            manDistance.setDistanceFunction(new ChebyshevDistance());
+            LinearNNSearch filDistance = new LinearNNSearch();
+            manDistance.setDistanceFunction(new FilteredDistance());
+            LinearNNSearch minkDistance = new LinearNNSearch();
+            manDistance.setDistanceFunction(new MinkowskiDistance());
+            LinearNNSearch[] distances = new LinearNNSearch[]{manDistance, euDistance, cheDistance, filDistance, minkDistance};
 
-        SelectedTag[] tags = new SelectedTag[]{new SelectedTag(IBk.WEIGHT_NONE, IBk.TAGS_WEIGHTING),new SelectedTag(IBk.WEIGHT_INVERSE, IBk.TAGS_WEIGHTING), new SelectedTag(IBk.WEIGHT_SIMILARITY, IBk.TAGS_WEIGHTING)};
+            SelectedTag[] tags = new SelectedTag[]{new SelectedTag(IBk.WEIGHT_NONE, IBk.TAGS_WEIGHTING), new SelectedTag(IBk.WEIGHT_INVERSE, IBk.TAGS_WEIGHTING), new SelectedTag(IBk.WEIGHT_SIMILARITY, IBk.TAGS_WEIGHTING)};
 
-        //Bestelako parametroak
-        Evaluation eval = null;
-        IBk ibk = new IBk();
-        Double maxf = 0.0;
-        int maxk=0;
-        LinearNNSearch maxd=null;
-        SelectedTag maxw=null;
+            //Bestelako parametroak
+            Evaluation eval = null;
+            IBk ibk = new IBk();
+            Double maxf = 0.0;
+            int maxk = 0;
+            LinearNNSearch maxd = null;
+            SelectedTag maxw = null;
 
 
-        //Prozesaketa
-        for (int k=1; k<=data.numInstances();k++){
-            ibk.setKNN(k);
-            for(LinearNNSearch d : distances){
-                ibk.setNearestNeighbourSearchAlgorithm(d);
-                for(SelectedTag w : tags){
-                    eval=new Evaluation(data);
-                    ibk.setDistanceWeighting(w);
-                    eval.crossValidateModel(ibk,data,10,new Random(1));
-                    Double fmeasure = eval.fMeasure(0);
-                    System.out.println("k = " + k + " distantzia = " + d + " eta pisua = " + w );
-                    if(maxf<fmeasure){
-                        maxf = fmeasure;
-                        maxk=k;
-                        maxd=d;
-                        maxw=w;
+            //Prozesaketa
+            for (int k = 1; k <= data.numInstances(); k++) {
+                ibk.setKNN(k);
+                for (LinearNNSearch d : distances) {
+                    ibk.setNearestNeighbourSearchAlgorithm(d);
+                    for (SelectedTag w : tags) {
+                        eval = new Evaluation(data);
+                        ibk.setDistanceWeighting(w);
+                        eval.crossValidateModel(ibk, data, 10, new Random(1));
+                        Double fmeasure = eval.fMeasure(0);
+                        System.out.println("k = " + k + " distantzia = " + d + " eta pisua = " + w);
+                        if (maxf < fmeasure) {
+                            maxf = fmeasure;
+                            maxk = k;
+                            maxd = d;
+                            maxw = w;
+                        }
                     }
                 }
             }
+            System.out.println("EMAITZAK: k-ren balio optimoa = " + maxk + " da, " + maxd + " distantziarekin eta " + maxw + " pisuarekin non fmeasure = " + maxf + " den.");
         }
-        System.out.println("EMAITZAK: k-ren balio optimoa = "+maxk+" da, "+maxd+" distantziarekin eta "+maxw+" pisuarekin non fmeasure = "+maxf+" den.");
     }
 }
