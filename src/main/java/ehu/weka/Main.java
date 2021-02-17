3483ce5737b4d6f94b1fc8b9fed6505b8929db4e
Jon Gondra <jongondra22@gmail.com>
2021-02-18 00:07:16 +0100

    Amaituta.

--- src/main/java/ehu/weka/Main.java
+++ src/main/java/ehu/weka/Main.java
@@ -2,7 +2,13 @@ package ehu.weka;
 
 import weka.classifiers.evaluation.Evaluation;
 import weka.classifiers.lazy.IBk;
-import weka.core.*;
+import weka.core.ChebyshevDistance;
+import weka.core.EuclideanDistance;
+import weka.core.FilteredDistance;
+import weka.core.Instances;
+import weka.core.ManhattanDistance;
+import weka.core.MinkowskiDistance;
+import weka.core.SelectedTag;
 import weka.core.converters.ConverterUtils;
 import weka.core.neighboursearch.LinearNNSearch;
 import weka.filters.Filter;
@@ -54,19 +60,20 @@ public class Main {
             LinearNNSearch manDistance = new LinearNNSearch();
             manDistance.setDistanceFunction(new ManhattanDistance());
             LinearNNSearch euDistance = new LinearNNSearch();
-            manDistance.setDistanceFunction(new EuclideanDistance());
+            euDistance.setDistanceFunction(new EuclideanDistance());
             LinearNNSearch cheDistance = new LinearNNSearch();
-            manDistance.setDistanceFunction(new ChebyshevDistance());
+            cheDistance.setDistanceFunction(new ChebyshevDistance());
             LinearNNSearch filDistance = new LinearNNSearch();
-            manDistance.setDistanceFunction(new FilteredDistance());
+            filDistance.setDistanceFunction(new FilteredDistance());
             LinearNNSearch minkDistance = new LinearNNSearch();
-            manDistance.setDistanceFunction(new MinkowskiDistance());
+            minkDistance.setDistanceFunction(new MinkowskiDistance());
             LinearNNSearch[] distances = new LinearNNSearch[]{manDistance, euDistance, cheDistance, filDistance, minkDistance};
 
             SelectedTag[] tags = new SelectedTag[]{new SelectedTag(IBk.WEIGHT_NONE, IBk.TAGS_WEIGHTING), new SelectedTag(IBk.WEIGHT_INVERSE, IBk.TAGS_WEIGHTING), new SelectedTag(IBk.WEIGHT_SIMILARITY, IBk.TAGS_WEIGHTING)};
 
             //Bestelako parametroak
             Evaluation eval = null;
+            Evaluation maxeval = new Evaluation(data);
             IBk ibk = new IBk();
             Double maxf = 0.0;
             int maxk = 0;
@@ -74,33 +81,53 @@ public class Main {
             SelectedTag maxw = null;
             int index = 0;
             int maxindex = 0;
+
             //Prozesaketa
-            for (int k = 1; k <= data.numInstances(); k++) {
+            for (int k = 1; k <= data.numInstances()*0.9; k++) {
                 ibk.setKNN(k);
+                index = 0;
                 for (LinearNNSearch d : distances) {
-                    if(index==distances.length){
-                        index = 0;
-                    }
                     ibk.setNearestNeighbourSearchAlgorithm(d);
+                    int pisua = 0;
                     for (SelectedTag w : tags) {
-                        eval = new Evaluation(data);
-                        ibk.setDistanceWeighting(w);
-                        eval.crossValidateModel(ibk, data, 10, new Random(1));
-                        Double fmeasure = eval.fMeasure(0);
-                        System.out.println("k = " + k + " distantzia = " + printeatudistantzia(index) + " eta pisua = " + w);
-                        if (maxf < fmeasure) {
-                            maxf = fmeasure;
-                            maxk = k;
-                            maxd = d;
-                            maxw = w;
-                            maxindex = index;
+                        try {
+                            eval = new Evaluation(data);
+                            ibk.setDistanceWeighting(w);
+                            eval.crossValidateModel(ibk, data, 10, new Random(1));
+                            Double fmeasure = eval.weightedFMeasure();
+                            System.out.println("k = " + k + " distantzia = " + printeatudistantzia(index) + " eta pisua = " +  printeatupisua(pisua));
+                            pisua++;
+                            if (maxf < fmeasure) {
+                                maxf = fmeasure;
+                                maxk = k;
+                                maxd = d;
+                                maxw = w;
+                                maxindex = index;
+                                maxeval = eval;
+                            }
+                        }
+                        catch (Exception e) {
+                            System.out.println("k = " + k + " distantzia = " + printeatudistantzia(index) + " eta pisua = " + printeatupisua(pisua) + " ERROREA");
                         }
                     }
                     index ++;
                 }
             }
             System.out.println("EMAITZAK: k-ren balio optimoa = " + maxk + " da, " + printeatudistantzia(maxindex)+ " distantziarekin eta " + maxw + " pisuarekin non fmeasure = " + maxf + " den.");
+            System.out.println(maxeval.toMatrixString());
         }
+    }
+
+    private static String printeatupisua(int pisua) {
+        switch(pisua) {
+            case 0:
+                return "None";
+            case 1:
+                return "Inverse";
+            case 2:
+                return "Similarity";
+        }
+        return null;
     }
 
     private static String printeatudistantzia(int i) {
